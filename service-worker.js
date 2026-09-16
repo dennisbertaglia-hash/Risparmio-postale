c244d1b7b1069f21f03326d3e58e2c11540e0edc
dennisbertaglia-hash <dennisbertaglia@gmail.com>
2026-09-16 15:59:44 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "risparmio-postale-v5";
+const CACHE_NAME = "risparmio-postale-v6";
 
 const FILES_TO_CACHE = [
   "./",
@@ -32,10 +32,24 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-        return cachedResponse || fetch(event.request);
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
       })
   );
 });
