56019aaeee0ead2d834ca795d58dcd5844744501
dennisbertaglia-hash <dennisbertaglia@gmail.com>
2026-09-16 15:37:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,51 +9,33 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
-
   self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
   );
-
 });
 
-
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
     caches.keys()
       .then(cacheNames => {
-
         return Promise.all(
-
           cacheNames
             .filter(name => name !== CACHE_NAME)
             .map(name => caches.delete(name))
-
         );
-
       })
       .then(() => self.clients.claim())
-
   );
-
 });
 
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request)
       .then(cachedResponse => {
-
         return cachedResponse || fetch(event.request);
-
       })
-
   );
-
 });
