3b593d9432ff172494d6390b4cab60b967515537
dennisbertaglia-hash <dennisbertaglia@gmail.com>
2026-09-16 11:40:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "risparmio-postale-v2";
+const CACHE_NAME = "risparmio-postale-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -9,33 +9,51 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+
 });
+
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    }).then(() => {
-      return self.clients.claim();
-    })
+
+    caches.keys()
+      .then(cacheNames => {
+
+        return Promise.all(
+
+          cacheNames
+            .filter(name => name !== CACHE_NAME)
+            .map(name => caches.delete(name))
+
+        );
+
+      })
+      .then(() => self.clients.claim())
+
   );
+
 });
 
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
+
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        return cachedResponse || fetch(event.request);
+
+      })
+
   );
+
 });
